b6a2844d7653fc502f586ff105f78305e38ae81d
Rishabh <rishabhrajshrm00@gmail.com>
2020-08-03 21:06:14 +0530

    Sizing in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.red,
+                child: Text('Hello Moto')),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+            Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.blue,
+                child: Text('Hello Realme')),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
