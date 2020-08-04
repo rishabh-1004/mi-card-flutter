8e045eb81a0a148339f9984e8fe25dc622694523
Rishabh <rishabhrajshrm00@gmail.com>
2020-08-04 23:35:42 +0530

    fonts and Icons

--- lib/main.dart
+++ lib/main.dart
@@ -14,36 +14,96 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.red,
-                child: Text('Hello Moto')),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  height: 100.0,
-                  width: 100.0,
-                  color: Colors.yellow,
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.teal,
+                backgroundImage: AssetImage('images/rishabh.jpeg'),
+              ),
+              Container(
+                child: Text(
+                  'Rishabh Raj Sharma',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
                 ),
-                Container(
-                  height: 100.0,
-                  width: 100.0,
-                  color: Colors.green,
+              ),
+              Container(
+                child: Text(
+                  'Flutter Developer',
+                  style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      letterSpacing: 2.0),
                 ),
-              ],
-            ),
-            Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.blue,
-                child: Text('Hello Realme')),
-          ],
-        )),
+              ),
+              SizedBox(
+                child: Divider(
+                  height: 10,
+                  thickness: 5,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      size: 30,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      ' rishabhrajshrm00@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.teal.shade900,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      size: 30,
+                      color: Colors.teal.shade900,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      ' +91 9078673199',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.teal.shade900,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
